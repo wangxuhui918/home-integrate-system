4222fc82324475853b5ae22cee799916e1da72d6
汪旭辉 <wangxuhui918@163.com>
2022-07-21 09:47:45 +0800

    重点修复日志

--- home-integrate-core/home-integrate-coreextend/home-integrate-systemconfig/src/main/java/com/guoshiyao/rely/systemconfig/StartPingService.java
+++ home-integrate-core/home-integrate-coreextend/home-integrate-systemconfig/src/main/java/com/guoshiyao/rely/systemconfig/StartPingService.java
@@ -52,7 +52,6 @@ public class StartPingService implements CommandLineRunner {
         LoggerBaseAb.info("默认jdbc_jar包路径 : {} [key]:[{}] ", Line.main_jdbc_jar_fullpath, "Line.main_jdbc_jar_fullpath");
         LoggerBaseAb.info("swagger 地址 : {} ", "http://127.0.0.1:" + Line.setting.get("system.servlet.port") + "/swagger-ui/index.html");
         System.out.println(ResourceUtil.readUtf8Str("ban.txt"));
-        LoggerBaseAb.info("启动完成[{}]", "SpingBoot");
     }
 }
 
