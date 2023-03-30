fa675d3bda9b054b274696e415a336ea6ffc8406
汪旭辉 <wangxuhui918@163.com>
2023-03-30 17:36:27 +0800

    增加描述说明

--- micro-framework/micro-bigcore/src/main/java/cn/bigcore/micro/core/configration/home/bean/ConfigDetailsVo.java
+++ micro-framework/micro-bigcore/src/main/java/cn/bigcore/micro/core/configration/home/bean/ConfigDetailsVo.java
@@ -11,24 +11,38 @@ package cn.bigcore.micro.core.configration.home.bean;
 
 import java.io.Serializable;
 
-
+/**
+ * properties文件描述类
+ */
 public class ConfigDetailsVo implements Serializable {
 
-
+    /**
+     * 参数ID
+     */
     private Integer id;
-
-
+    /**
+     * 参数名
+     */
     private String key;
+    /**
+     * 参数前缀
+     */
     private String beforesuff;
-
-
+    /**
+     * 切割符号,比如=
+     */
     private String m;
-
-
+    /**
+     * 参数值
+     */
     private String value;
-
+    /**
+     * 参数备注
+     */
     private String mark;
-
+    /**
+     * 当前参数文件名
+     */
     private String name_en;
 
     public String getBeforesuff() {
