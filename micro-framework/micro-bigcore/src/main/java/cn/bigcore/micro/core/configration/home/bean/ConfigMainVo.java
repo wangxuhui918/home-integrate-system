d7dea84065025373f4ab6d8a508f7a6096467247
汪旭辉 <wangxuhui918@163.com>
2023-05-23 15:59:36 +0800

    修改核心配置类

--- micro-framework/micro-bigcore/src/main/java/cn/bigcore/micro/core/configration/home/bean/ConfigMainVo.java
+++ micro-framework/micro-bigcore/src/main/java/cn/bigcore/micro/core/configration/home/bean/ConfigMainVo.java
@@ -9,11 +9,14 @@
 package cn.bigcore.micro.core.configration.home.bean;
 
 
+import lombok.Data;
+
 import java.io.Serializable;
 
 /**
  * properties文件描述类
  */
+@Data
 public class ConfigMainVo implements Serializable {
     /**
      * 文件名
@@ -41,52 +44,5 @@ public class ConfigMainVo implements Serializable {
     private boolean need_format_zone;
 
 
-    public String getConfigFileName() {
-        return configFileName;
-    }
-
-    public void setConfigFileName(String configFileName) {
-        this.configFileName = configFileName;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getContext() {
-        return context;
-    }
-
-    public void setContext(String context) {
-        this.context = context;
-    }
-
-    public boolean isOnly_local() {
-        return only_local;
-    }
-
-    public void setOnly_local(boolean only_local) {
-        this.only_local = only_local;
-    }
-
-    public boolean isUse_uk() {
-        return use_uk;
-    }
-
-    public void setUse_uk(boolean use_uk) {
-        this.use_uk = use_uk;
-    }
-
-    public boolean isNeed_format_zone() {
-        return need_format_zone;
-    }
-
-    public void setNeed_format_zone(boolean need_format_zone) {
-        this.need_format_zone = need_format_zone;
-    }
 }
 
