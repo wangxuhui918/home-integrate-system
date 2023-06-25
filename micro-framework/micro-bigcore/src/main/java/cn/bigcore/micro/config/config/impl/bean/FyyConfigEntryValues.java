1a0991a616fa5750ca32d6f409d0ffadcc82a143
汪旭辉 <wangxuhui918@163.com>
2023-06-25 17:33:54 +0800

    调整

--- micro-framework/micro-bigcore/src/main/java/cn/bigcore/micro/config/config/impl/bean/FyyConfigEntryValues.java
+++ micro-framework/micro-bigcore/src/main/java/cn/bigcore/micro/config/config/impl/bean/FyyConfigEntryValues.java
@@ -21,16 +21,12 @@ public enum FyyConfigEntryValues {
     SYSTEMEXTEND("systemextend.ini", ""),//
     MESSAGE("message-${i18n}.xml", "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?>\n" +
             "<message>\n" +
-            "\n" +
-            "\t<member code=\"DEMO01\" conmtext=\"客户基础数据维护成功\" type=\"true\" />\n" +
-            "\n" +
-            "\t<group name=\"${idKey}\">\n" +
-            "\t\t<member code=\"01\" conmtext=\"获取数据成功\" type=\"true\" />\n" +
-            "\t\t<member code=\"02\" conmtext=\"主键目前只能有一个!!\" type=\"false\" />\n" +
-            "\t\t<member code=\"03\" conmtext=\"{}为不支持的数据库类型\" type=\"false\" />\n" +
-            "\t</group>\n" +
-            "\n" +
-            "\n" +
+            "\t\t<member code=\"DEMO01\" conmtext=\"客户基础数据维护成功\" type=\"true\" />\n" +
+            "\t\t<group name=\"${idKey}\">\n" +
+            "\t\t\t\t<member code=\"01\" conmtext=\"获取数据成功\" type=\"true\" />\n" +
+            "\t\t\t\t<member code=\"02\" conmtext=\"主键目前只能有一个!!\" type=\"false\" />\n" +
+            "\t\t\t\t<member code=\"03\" conmtext=\"{}为不支持的数据库类型\" type=\"false\" />\n" +
+            "\t\t</group>\n" +
             "</message>"),//
 
 
