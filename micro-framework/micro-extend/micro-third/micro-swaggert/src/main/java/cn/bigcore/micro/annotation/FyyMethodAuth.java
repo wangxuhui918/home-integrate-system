3814b2492954e9cd57487b2f7faad5fe98a28f27
汪旭辉 <wangxuhui918@163.com>
2023-07-17 17:42:04 +0800

    checkauth增加默认method入参

---
+++ micro-framework/micro-extend/micro-third/micro-swaggert/src/main/java/cn/bigcore/micro/annotation/FyyMethodAuth.java
@@ -0,0 +1,12 @@
+
+package cn.bigcore.micro.annotation;
+
+import java.lang.annotation.*;
+
+
+@Target({ElementType.TYPE, ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface FyyMethodAuth {
+    String[] value() default "";
+}
