3e93919afa0508fdd0779e3278c5ee222fae8348
汪旭辉 <wangxuhui918@163.com>
2024-03-13 08:26:56 +0800

    * 这是空一场处理类,表示某些特殊情况下抛出的异常

--- micro-framework/micro-bigcore/src/main/java/cn/bigcore/micro/base/exception/FyyExceptionApiNull.java
+++ micro-framework/micro-bigcore/src/main/java/cn/bigcore/micro/base/exception/FyyExceptionApiNull.java
@@ -13,6 +13,7 @@ package cn.bigcore.micro.base.exception;
 
 /**
  * 仅用于某些情况下用于判断是否为本系统抛出的空异常
+ * 这是空一场处理类,表示某些特殊情况下抛出的异常
  */
 public class FyyExceptionApiNull extends RuntimeException {
     public FyyExceptionApiNull() {
